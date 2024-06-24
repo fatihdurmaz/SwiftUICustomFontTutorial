848a56a9224475e0995fe15ae228087284867106
Fatih DURMAZ <fatih.durmaz@outlook.com>
2024-06-24 14:09:30 +0300

    Initial Commit

---
+++ CustomFontTutorial/CustomFontTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomFontTutorialApp.swift
+//  CustomFontTutorial
+//
+//  Created by Fatih Durmaz on 24.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct CustomFontTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
